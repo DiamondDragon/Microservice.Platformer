762cc779e99cd92948907e8321d126235e2241f8
Andei Salanoi <a.salanoi@godeltech.com>
2018-08-23 12:17:47 +0300

    Add project files.

---
+++ Microservice.Platformer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Microservice.Platformer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
